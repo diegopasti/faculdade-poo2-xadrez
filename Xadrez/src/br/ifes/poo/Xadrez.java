9ecbf4e186cfa0d78c8c9cc6996d46ee03afbef6
Diego Pasti <diegopasti@gmail.com>
2014-06-05 17:55:31 -0300

    Commit + push feito para verificar configuração.

--- Xadrez/src/br/ifes/poo/Xadrez.java
+++ Xadrez/src/br/ifes/poo/Xadrez.java
@@ -5,6 +5,7 @@ import br.ifes.poo.controle.controle_interface.ControleAplicacao;
 public class Xadrez {
 
 	public static void main(String[] args) {
+		// TESTE
 		System.out.println("INICIANDO O XADREZ");
 		ControleAplicacao Xadrez = new ControleAplicacao();
 		Xadrez.iniciarAplicativo(); 
